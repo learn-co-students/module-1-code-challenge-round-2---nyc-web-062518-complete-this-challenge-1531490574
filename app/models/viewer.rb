550a2940f995d46eff4dba08cae63abe82665d53
Jack Yeung <jyeung.se@gmail.com>
2018-07-13 11:03:38 -0400

    done.

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,21 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def queue_items
+    QueueItem.all.select {|queue_item| queue_item.viewer == self}
+  end
+
+  def queue_movies
+    queue_items.map {|queue_item| queue_item.movie}
+  end
+
+  def add_movie_to_queue(movie)
+    QueueItem.new(nil, self, movie)
+  end
+
+  def rate_movie(movie, rating)
+
+  end
+
 end
