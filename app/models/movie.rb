b730409b8ba647e5168bb12a45e3812077990992
seanraad123 <seanconrad123@gmail.com>
2018-07-13 11:04:46 -0400

    done!

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie
-  attr_accessor :title
+  attr_accessor :title, :rating
 
   @@all = []
 
@@ -10,6 +10,37 @@ class Movie
 
   def self.all
     @@all
+  end
+
+
+  def queue_items
+  	QueueItem.all.select do |item|
+  		item.movie == self
+  	end
+  end
+
+  def viewers
+  	queue_items.map do |item|
+  		item.viewer
+  	end
+  end
+
+  def average_rating
+  	total_rating = 0
+  	binding.pry
+  	queue_items.map do |item|
+  		total_rating += item.rating
+  	end
+  	total_rating/queue_items.length
+  end
+
+
+  def self.highest_rated
+  	array = []
+  	all.map do |movie|
+  		array << movie.average_rating
+  	end
+  	array.max
   end
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -5,11 +5,45 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
+ #return an array of `QueueItem` instances
+  def queue_items
+  	QueueItem.all.select do |item|
+  		item.viewer == self
+  	end
+  end
+
+# return an array of `Movie` instances in the `Viewer`'s queue.
+  def queue_movies
+  	queue_items.map do |item|
+  		item.movie
+  	end
+  end
+
+#receive a `Movie` instance as its only argument and add it to the `Viewer`'s queue
+  def add_movie_to_queue(movie)
+  	QueueItem.new(self, movie, nil)
+  end
+
+
+  def rate_movie(movie, rating)
+  	# If it is in the queue
+  	if queue_items.include?(movie)
+  		item = queue_items.find do |queue_item|
+  			queue_item.movie == movie
+  		end
+  		item.rating = rating
+  	
+  	#If it is not in the queue
+  	else
+  		QueueItem.new(self, movie, rating)
+  	end
+  end
+
 end
