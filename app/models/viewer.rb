a34c01afe1ca4fbb5e76dcca9bdcd3af77aebd24
github username <github email address>
2018-07-13 11:02:44 -0400

    done

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Viewer
   attr_accessor :username
 
@@ -6,10 +8,32 @@ class Viewer
   def initialize(username)
     @username = username
     self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
+  def queue_items
+    QueueItem.all.select do |queue_item|
+      queue_item.viewer == self
+    end
+  end
+
+  def queue_movies
+    queue_items.map do |queue_item|
+      queue_item.movie
+    end
+  end
+
+  def add_movie_to_queue(movie)
+    QueueItem.new(self, movie)
+  end
+
+  def rate_movie(movie, rating)
+    QueueItem.new(self, movie, rating)
+  end
+
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,14 @@
 require 'bundler/setup'
 Bundler.require
 require_rel '../app'
+
+incredibles = Movie.new("Incredibles")
+kungfupanda = Movie.new("Kung Fu Panda")
+
+ann = Viewer.new("Ann")
+jack = Viewer.new("Jack")
+sean = Viewer.new("Sean")
+
+item1 = QueueItem.new(ann, incredibles, 4)
+item2 = QueueItem.new(jack, kungfupanda, 5)
+item3 = QueueItem.new(sean, incredibles, 5)
