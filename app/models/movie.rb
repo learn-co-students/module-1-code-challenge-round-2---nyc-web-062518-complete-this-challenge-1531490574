a062d09e190cbbfe83b64d3e70a976d1b670f0d4
Tony Turetsky <TTuretsky@gmail.com>
2018-07-13 11:04:12 -0400

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,31 @@ class Movie
     @@all
   end
 
+  def queue_items
+    QueueItem.all.select do |queue|
+      queue.movie == self
+    end
+  end
+
+  def viewers
+    queue_items.map do |movie|
+      movie.viewer
+    end
+  end
+
+  def average_rating
+    ratings_var = 0
+    queue_items.each do |movie|
+      ratings_var += movie.rating
+    end
+    ratings_var / queue_items.length
+  end
+
+  def self.highest_rated
+    movie_ordered = all.max do |a,b|
+      a.average_rating <=> b.average_rating
+    end
+    movie_ordered[0]
+  end
+
 end
